38c64f49f05a67fa96f200e50e5271270aa1ffce
phucgo240699 <phucgo240699@gmail.com>
2021-12-09 14:07:39 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express")
+const fileUpload = require("express-fileupload")
+const { AwakeHeroku } = require("awake-heroku");
+const path = require('path')
+
+const app = express()
+AwakeHeroku.add("https://fahabo-video-storage.herokuapp.com");
+
+const PORT = process.env.PORT || 3002
+
+app.use(fileUpload())
+app.use(express.static(path.resolve(__dirname, 'public')))
+
+app.post("/upload", async (req, res, next) => {
+  try {
+    if (!req.files || Object.keys(req.files).length === 0) {
+      res.status(400).send("No files were found")
+    }
+    else {
+      const file = req.files.file
+      const fileName = `cuisine_video_${(new Date()).getTime().toString()}.mp4`
+      await file.mv(`${__dirname}/public/uploads/${fileName}`, function(err) {
+        if (err) {
+          res.status(400).send(err)
+        }
+        else {
+          res.status(200).send(fileName)
+        }
+      })
+    }
+  } catch (error) {
+    res.status(500).send(error)
+  }
+})
+
+app.listen(PORT, () => {
+  AwakeHeroku.start();
+})
